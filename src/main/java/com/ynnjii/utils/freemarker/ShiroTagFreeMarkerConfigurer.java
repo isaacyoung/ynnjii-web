6ec72f2c38ac09f3cb23d2aa493d96c927ed908a
yzh <48515824@qq.com>
2017-05-25 16:59:18 +0800

    sys

---
+++ src/main/java/com/ynnjii/utils/freemarker/ShiroTagFreeMarkerConfigurer.java
@@ -0,0 +1,22 @@
+package com.ynnjii.utils.freemarker;
+
+import com.jagregory.shiro.freemarker.ShiroTags;
+import freemarker.template.TemplateException;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import java.io.IOException;
+
+/**
+ * project：ynnjii-web
+ * class：
+ * author：isaac
+ * date：2017/5/25
+ * description：
+ */
+public class ShiroTagFreeMarkerConfigurer extends FreeMarkerConfigurer {
+    @Override
+    public void afterPropertiesSet() throws IOException, TemplateException {
+        super.afterPropertiesSet();
+        this.getConfiguration().setSharedVariable("shiro", new ShiroTags());
+    }
+}
